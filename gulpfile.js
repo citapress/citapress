af528004d9b67ec7b7b8973b4113bea183b674d3
J.J. Castro <jj.castro10@uniandes.edu.co>
2017-10-10 10:42:51 -0500

    Agregar cositas

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp       = require('gulp');
+var livereload = require('gulp-livereload');
+var rev        = require('gulp-rev-append');
+
+// TASKS FOR LIVE RELOADING ==========
+gulp.task('css', function() {
+  gulp.src('*.css').pipe(livereload());
+});
+
+gulp.task('js', function() {
+  gulp.src('js/*.js').pipe(livereload());
+});
+
+gulp.task('html', function() {
+  gulp.src(['*.html', 'views/*.html']).pipe(livereload())
+});
+
+gulp.task('rev', function() {
+  gulp.src('./index.html')
+    .pipe(rev())
+    .pipe(gulp.dest('.'));
+});
+
+// WATCH TASK =========================
+gulp.task('watch', function() {
+  livereload.listen();
+
+  gulp.watch(['*.html', 'views/*.html'], ['html', 'rev']);
+  gulp.watch('*.css', ['css']);
+  gulp.watch('js/*.js', ['js']);
+});
+
+
+gulp.task('default', ['watch']);
