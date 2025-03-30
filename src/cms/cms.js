b4cd1930ac310f8df2e9c176e8ce79226f884098
fabianrios <hola@fabianrios.co>
2025-03-30 18:11:45 +0200

    uploadcare again

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,6 @@ import CMS from 'decap-cms-app';
 import uploadcare from "decap-cms-media-library-uploadcare";
 
 CMS.registerMediaLibrary(uploadcare);
-// In your React component or main application file
-console.log('All environment variables:', process.env);
+console.log('UPLOAD_CARE_API:', process.env.GATSBY_UPLOAD_CARE_API);
+console.log('ALL:', process.env);
 
