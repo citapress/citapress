0fd5a2058f1c910c62e0c19682f0f36ca090bd33
fabianrios <hola@fabianrios.co>
2025-03-30 18:55:37 +0200

    uploadcare pass it on cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,7 @@
 import CMS from 'decap-cms-app';
 import uploadcare from "decap-cms-media-library-uploadcare";
 
-CMS.registerMediaLibrary(uploadcare);
-console.log('UPLOAD_CARE_API:', process.env.GATSBY_UPLOAD_CARE_API);
+const opt = { publicKey: process.env.GATSBY_UPLOAD_CARE_API };
+CMS.registerMediaLibrary(uploadcare, opt);
+console.log({opt});
 
